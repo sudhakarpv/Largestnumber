3ecd3b432f876d48be42ea2982941e639d441e6a
sudhakarpv <40256682+sudhakarpv@users.noreply.github.com>
2018-06-14 16:41:44 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+num1= input("Enter Number")
+num2= input("Enter Number")
+num3= input("Enter Number")
+if(num1>=num2 and num1>=num3):
+    print(num1,"is Largest Number")
+elif(num2>num1 and num1>=num3):
+    print(num2,"is Largest Number")
+else:
+    print(num3,"is Largest Number")
